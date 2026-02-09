85bbd0824971d0553dc3f31e3ebc0506f33416c7
Sanjeev Midda <s.midda1@gmail.com>
2026-02-09 21:14:29 +0000

    created header components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  // create function for add task
+  // create function for moving task to active
+  // create function for moving task to completed
+  // create function to delete
+
+  return (
+    <div className="App">
+      {/* // create title */}
+
+      <header>
+        <div className="circle"></div>
+        <input type="text" placeholder="Enter Task" />
+      </header>
+
+      {/* // create component for number of items */}
+
+      {/* // create Content area */}
+
+      {/* // create section container component */}
+    </div>
+  );
 }
 
 export default App;
