27088c401e86f8d863ada806089ec16fd37a97ee
Sanjeev Midda <s.midda1@gmail.com>
2026-02-09 21:40:29 +0000

    created UI for content area

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./index.css";
+import MainTaskContainer from "./components/MainTaskContainer";
 
 function App() {
   // create function for add task
   // create function for moving task to active
   // create function for moving task to completed
   // create function to delete
+  // add data
 
   return (
     <div className="App">
@@ -17,7 +19,14 @@ function App() {
 
       {/* // create component for number of items */}
 
+      {/* // Create Habit component */}
       {/* // create Content area */}
+
+      <div className="contentArea">
+        <MainTaskContainer sectionName="CURRENT" currentNumbers={`(4)`} />
+        <MainTaskContainer sectionName="ACTIVE" currentNumbers={`(3)`} />
+        <MainTaskContainer sectionName="COMPLETED" currentNumbers={`(2)`} />
+      </div>
 
       {/* // create section container component */}
     </div>
